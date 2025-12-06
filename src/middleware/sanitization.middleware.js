0bb7741cc29f73e33dc45d9ff036adf3db0c163f
anilpoudel07 <pas078bei004@wrc.edu.np>
2025-12-06 12:34:09 +0545

    Handliung bug in sanitize.middleware.js

--- src/middleware/sanitization.middleware.js
+++ src/middleware/sanitization.middleware.js
@@ -1,25 +1,29 @@
-// src/middleware/sanitization.middleware.js
+
+
+const clean = (obj) => {
+  if (!obj || typeof obj !== "object") return obj;
+
+  for (const key in obj) {
+   
+    if (key.startsWith("$") || key.includes(".")) {
+      delete obj[key];
+      continue;
+    }
+
+    if (obj[key] && typeof obj[key] === "object") {
+      clean(obj[key]);
+    }
+  }
+};
 
 export const sanitize = (req, res, next) => {
-  function clean(obj) {
-    if (!obj || typeof obj !== "object") return obj;
+ 
+  if (req.body) req.body = clean(req.body) || req.body;
+  if (req.params) req.params = clean(req.params) || req.params;
 
-    for (const key in obj) {
-      if (key.startsWith("$") || key.includes(".")) {
-        delete obj[key];
-        continue;
-      }
-      if (obj[key] && typeof obj[key] === "object") {
-        clean(obj[key]);
-      }
-    }
-    return obj;
+  if (req.query) {
+    clean(req.query);
   }
-
-
-  if (req.body) req.body = clean(req.body);
-  if (req.query) req.query = clean(req.query);
-  if (req.params) req.params = clean(req.params);
 
   next();
 };
