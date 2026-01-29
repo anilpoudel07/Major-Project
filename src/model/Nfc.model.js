0eadb62e366c52797c777e3886f89516f75ed8f4
anilpoudel07 <pas078bei004@wrc.edu.np>
2026-01-29 14:57:17 +0545

    my save account

--- src/model/Nfc.model.js
+++ src/model/Nfc.model.js
@@ -2,36 +2,37 @@
 import mongoose from "mongoose";
 
 const nfcCardSchema = new mongoose.Schema({
-  cardUid: {
-    type: String,
-    required: true,
-    unique: true,
-    uppercase: true,
-    trim: true,
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  cardType: {
-    type: String,
-    enum: ["personal", "student", "senior", "temporary"],
-    default: "personal",
-  },
-  isVerified: {
-    type: Boolean,
-    default: false,   
-  },
-  verifiedBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
-  verifiedAt: Date,
-  requestedAt: {
-    type: Date,
-    default: Date.now,
-  },
+    cardUid: {
+        type: String,
+        required: true,
+        unique: true,
+        uppercase: true,
+        trim: true,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    cardType: {
+        type: String,
+        enum: ["personal", "student", "senior", "temporary"],
+        default: "personal",
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    verifiedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    verifiedAt: Date,
+    requestedAt: {
+        type: Date,
+        default: Date.now,
+    },
 });
 
 export const NfcCard = mongoose.model("NfcCard", nfcCardSchema);
+
